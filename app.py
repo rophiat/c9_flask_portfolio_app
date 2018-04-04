8a4fcfc95fd065027d6dada663bc58060be50ce5
rophiat <37983990+rophiat@users.noreply.github.com>
2018-04-04 11:28:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-	new_name = name + "Likes to eat mangos"
+	new_name = str(name) + "Likes to eat mangos"
 	return render_template('index.html')
 
 @app.route('/<name>')
